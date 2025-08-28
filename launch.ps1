d051ad8b99c6e9bf2b301115568ee7558ad7f2c2
Shariar Ratul <alshariarratul@gmail.com>
2025-08-28 12:40:02 +0600

    Update launch.ps1

--- launch.ps1
+++ launch.ps1
@@ -1,5 +1,8 @@
+# Get current user
+$userName = $env:USERNAME
+
 # Navigate to your project folder
-Set-Location "C:\Users\CSE\AppData\Local\ycd"
+Set-Location "C:\Users\$userName\AppData\Local\ycd"
 
 # Start node server in background
 Start-Process "node" -ArgumentList "server.js" -WindowStyle Hidden
